46fea28cfe5d28c01a3937e6ce6d0444720064f8
Joeumar <joeumar@gmail.com>
2016-05-02 16:05:15 -0300

    alteracoes

--- js/dados.js
+++ js/dados.js
@@ -1,71 +1,76 @@
 var app = angular.module('app', []);
 
-app.controller('libraryController', function ($scope) {
-        $scope.bookList = [
-                {id: 1, titulo: 'bla', autores: 'ble', descricao: 'bli', imagem: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Open_book_01.svg/50px-Open_book_01.svg.png', comentarios: ['exemplo de comentario'], preco: 'bla'},
-                {id: 2, titulo: 'ble', autores: 'ble', descricao: 'bli', imagem: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Open_book_01.svg/50px-Open_book_01.svg.png', comentarios: [], preco: 'bla'},
-                {id: 3, titulo: 'bli', autores: 'ble', descricao: 'bli', imagem: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Open_book_01.svg/50px-Open_book_01.svg.png', comentarios: [], preco: 'bla'},
-                {id: 4, titulo: 'blo', autores: 'ble', descricao: 'bli', imagem: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Open_book_01.svg/50px-Open_book_01.svg.png', comentarios: [], preco: 'bla'},
-                {id: 5, titulo: 'blu', autores: 'ble', descricao: 'bli', imagem: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Open_book_01.svg/50px-Open_book_01.svg.png', comentarios: [], preco: 'bla'},
-                {id: 6, titulo: 'cla', autores: 'ble', descricao: 'bli', imagem: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Open_book_01.svg/50px-Open_book_01.svg.png', comentarios: [], preco: 'bla'},
-                {id: 7, titulo: 'cle', autores: 'ble', descricao: 'bli', imagem: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Open_book_01.svg/50px-Open_book_01.svg.png', comentarios: [], preco: 'bla'},
-                {id: 8, titulo: 'cli', autores: 'ble', descricao: 'bli', imagem: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Open_book_01.svg/50px-Open_book_01.svg.png', comentarios: [], preco: 'bla'},
-                {id: 9, titulo: 'clo', autores: 'ble', descricao: 'bli', imagem: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Open_book_01.svg/50px-Open_book_01.svg.png', comentarios: [], preco: 'bla'},
-                {id: 10, titulo: 'clu', autores: 'ble', descricao: 'bli', imagem: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Open_book_01.svg/50px-Open_book_01.svg.png', comentarios: [], preco: 'bla'},
-        ];
+app.controller('libraryController', function () {
+    var libraryController = this;
 
-        $scope.commentEnabled = false;
-        $scope.editData = {};
+    this.bookList = [{title: 'bla', author: 'ble', description: 'bli', cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Open_book_01.svg/50px-Open_book_01.svg.png', comments: ['exemplo de comentario'], price: 'bla', edit: false}
+            ];
 
-        for (var i=0, lenght = $scope.bookList.lenght; i < lenght; i++){
-        	$scope.editData[$scope.bookList[i].id] = false;
-            //$scope.editData[$scope.bookList[i].id].comentario = false;
-        }
+    this.commentEnabled = false;
+    this.editData = {};
 
-        $scope.add = function(){
-                $scope.bookList.push({
-                		//id:$scope.bookList.lenght;
-                        titulo:$scope.titulo,
-                        autores:$scope.autores,
-                        descricao:$scope.descricao,
-                        imagem:$scope.imagem,
-                        comentarios:[],
-                        preco:$scope.preco
-                });
-                //Limpar Campos
-                $scope.titulo = '';
-                $scope.autores = '';
-                $scope.descricao = '';
-                $scope.imagem = '';
-                $scope.preco = '';
-            //cleanFields($scope.titulo, $scope.autores, $scope.descricao, $scope.imagem, $scope.comentario, $scope.preco);
-        }
+    for (var i=0, lenght = this.bookList.lenght; i < lenght; i++){
+        this.editData[this.bookList[i].edit] = false;
+        //this.editData[this.bookList[i].id].comentario = false;
+    }
 
-        $scope.modify = function(bookList){
-        	$scope.editData[bookList.id] = true;
-        }
+    this.add = function(){
+            this.bookList.push({
+                    //id:this.bookList.lenght;
+                    title:this.title,
+                    author:this.author,
+                    description:this.description,
+                    cover:this.cover,
+                    comments:[],
+                    price:this.price
+            });
+            //Limpar Campos
+            this.title = '';
+            this.author = '';
+            this.description = '';
+            this.cover = '';
+            this.price = '';
+        //cleanFields(this.title, this.author, this.description, this.cover, this.comentario, this.price);
+    }
 
-        $scope.update = function(bookList){
-        	$scope.editData[bookList.id] = false;
-        }
+    /*
+    this.modify = function(bookIndex){
+        book = this.bookList[bookIndex];
+        this.editData[book.edit] = true;
+    }
+    */
 
-        $scope.comment = function(book, x){
-            book.comentarios.push(x);
-        }
+    this.modify = function(bookList, index){
+        this.editData[this.bookList[index].edit] = true;
+    }
 
-        $scope.remove = function(bookIndex){
-            $scope.bookList.splice(bookIndex, 1);
-        }
+    this.update = function(bookList){
+        this.editData[bookList.id] = false;
+    }
 
-        $scope.edit = function(x){
-            alert(x.id);
-        }
+    this.comment = function(book, x){
+        book.comments.push(x);
+    }
+
+    this.remove = function(bookIndex){
+        //alert(this.bookList[bookIndex]);
+        this.bookList.splice(bookIndex, 1);
+    }
+
+    this.edit = function(x){
+        alert(x.id);
+    }
 
 
-        function cleanFields(){
-                for (var i=0; i < arguments.lenght; i++){
-                        alert(arguments[i]);
-                }
-        }
+    function getBookByIndex(bookIndex) {
+        alert(this.bookList[bookIndex]);
+        //return this.bookList[bookIndex];
+    }
+
+    function cleanFields(){
+            for (var i=0; i < arguments.lenght; i++){
+                    alert(arguments[i]);
+            }
+    }
 
 });
