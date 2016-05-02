dc6d01fffd6afc7a8899c7dced888adee04b7115
joeu <joeumar@gmail.com>
2016-05-02 19:27:22 -0300

    Adjustments in controller (modify and update)

--- js/dados.js
+++ js/dados.js
@@ -3,49 +3,41 @@ var app = angular.module('app', []);
 app.controller('libraryController', function () {
     var libraryController = this;
 
-    this.bookList = [{title: 'bla', author: 'ble', description: 'bli', cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Open_book_01.svg/50px-Open_book_01.svg.png', comments: ['exemplo de comentario'], price: 'bla', edit: false}
+    this.bookList = [
             ];
 
     this.commentEnabled = false;
     this.editData = {};
 
     for (var i=0, lenght = this.bookList.lenght; i < lenght; i++){
-        this.editData[this.bookList[i].edit] = false;
-        //this.editData[this.bookList[i].id].comentario = false;
+        this.editData[i] = false;
     }
 
     this.add = function(){
-            this.bookList.push({
-                    //id:this.bookList.lenght;
-                    title:this.title,
-                    author:this.author,
-                    description:this.description,
-                    cover:this.cover,
-                    comments:[],
-                    price:this.price
-            });
-            //Limpar Campos
-            this.title = '';
-            this.author = '';
-            this.description = '';
-            this.cover = '';
-            this.price = '';
+        this.bookList.push({
+            //id:this.bookList.lenght;
+            title:this.title,
+            author:this.author,
+            description:this.description,
+            cover:this.cover,
+            comments:[],
+            price:this.price,
+        });
+        //Limpar Campos
+        this.title = '';
+        this.author = '';
+        this.description = '';
+        this.cover = '';
+        this.price = '';
         //cleanFields(this.title, this.author, this.description, this.cover, this.comentario, this.price);
     }
 
-    /*
-    this.modify = function(bookIndex){
-        book = this.bookList[bookIndex];
-        this.editData[book.edit] = true;
-    }
-    */
-
-    this.modify = function(bookList, index){
-        this.editData[this.bookList[index].edit] = true;
+    this.modify = function(index){
+        this.editData[index] = true;
     }
 
-    this.update = function(bookList){
-        this.editData[bookList.id] = false;
+    this.update = function(index){
+        this.editData[index] = false;
     }
 
     this.comment = function(book, x){
@@ -61,10 +53,8 @@ app.controller('libraryController', function () {
         alert(x.id);
     }
 
-
     function getBookByIndex(bookIndex) {
-        alert(this.bookList[bookIndex]);
-        //return this.bookList[bookIndex];
+        return this.bookList[bookIndex];
     }
 
     function cleanFields(){
